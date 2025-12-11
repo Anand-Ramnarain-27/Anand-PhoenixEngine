ac2400e9682ee13824d33614df552330787aac38
Anand-Ramnarain-27 <142887960+Anand-Ramnarain-27@users.noreply.github.com>
2025-12-11 20:28:55 +0100

    Add module swapping and update synchronization
    Introduces logic to swap modules during update, ensuring proper cleanup and initialization. Also wraps the update process in a guard to prevent re-entrancy and synchronizes rendering steps.

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -43,29 +43,51 @@ void Application::update()
 {
     using namespace std::chrono_literals;
 
-    // Update milis
-    uint64_t currentMilis = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-
-    elapsedMilis = currentMilis - lastMilis;
-    lastMilis = currentMilis;
-    tickSum -= tickList[tickIndex];
-    tickSum += elapsedMilis;
-    tickList[tickIndex] = elapsedMilis;
-    tickIndex = (tickIndex + 1) % MAX_FPS_TICKS;
-
-    if (!app->paused)
+    if (!updating)
     {
-        for (auto it = modules.begin(); it != modules.end(); ++it)
-            (*it)->update();
+        updating = true;
 
-        for (auto it = modules.begin(); it != modules.end(); ++it)
-            (*it)->preRender();
+        // Update milis
+        uint64_t currentMilis = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
-        for (auto it = modules.begin(); it != modules.end(); ++it)
-            (*it)->render();
+        elapsedMilis = currentMilis - lastMilis;
+        lastMilis = currentMilis;
+        tickSum -= tickList[tickIndex];
+        tickSum += elapsedMilis;
+        tickList[tickIndex] = elapsedMilis;
+        tickIndex = (tickIndex + 1) % MAX_FPS_TICKS;
 
-        for (auto it = modules.begin(); it != modules.end(); ++it)
-            (*it)->postRender();
+        if (!app->paused)
+        {
+            for (auto it = swapModules.begin(); it != swapModules.end(); ++it)
+            {
+                auto pos = std::find(modules.begin(), modules.end(), it->first);
+                if (pos != modules.end())
+                {
+                    (*pos)->cleanUp();
+                    delete* pos;
+
+                    it->second->init();
+                    *pos = it->second;
+                }
+            }
+
+            swapModules.clear();
+
+            for (auto it = modules.begin(); it != modules.end(); ++it)
+                (*it)->update();
+
+            for (auto it = modules.begin(); it != modules.end(); ++it)
+                (*it)->preRender();
+
+            for (auto it = modules.begin(); it != modules.end(); ++it)
+                (*it)->render();
+
+            for (auto it = modules.begin(); it != modules.end(); ++it)
+                (*it)->postRender();
+        }
+
+        updating = false;
     }
 }
 
