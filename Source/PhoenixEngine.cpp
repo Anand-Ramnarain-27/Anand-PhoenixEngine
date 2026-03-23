0b530d4a0c45a5cfd27b4a0f48aa282579fdeb22
Anand-Ramnarain-27 <142887960+Anand-Ramnarain-27@users.noreply.github.com>
2026-03-23 16:04:53 +0100

    Added Drag &  Drop

--- Source/PhoenixEngine.cpp
+++ Source/PhoenixEngine.cpp
@@ -10,6 +10,7 @@
 #include "ModuleAssets.h"
 
 #include <shellapi.h>
+#include <filesystem>
 
 #include "dxgidebug.h"
 
@@ -36,14 +37,12 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: Place code here.
 
     // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -56,7 +55,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
     // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
+    if (!InitInstance(hInstance, nCmdShow))
     {
         return FALSE;
     }
@@ -77,93 +76,142 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     delete app;
 
-
-    return (int) msg.wParam;
+    return (int)msg.wParam;
 }
 
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
-
     wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, NULL); 
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = NULL; 
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, NULL);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
     return RegisterClassExW(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+    hInst = hInstance;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+    if (!hWnd)
+        return FALSE;
 
-   DragAcceptFiles(hWnd, TRUE);
+    DragAcceptFiles(hWnd, TRUE);
 
-   app = new Application(__argc, __wargv, hWnd);
+    app = new Application(__argc, __wargv, hWnd);
 
-   if(!app->init())
-   {
-       delete app;
+    if (!app->init())
+    {
+        delete app;
+        return FALSE;
+    }
 
-       return FALSE;
-   }
+    // Set the window to be the size of the monitor
+    MONITORINFO monitor = {};
+    monitor.cbSize = sizeof(monitor);
+    GetMonitorInfo(MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST), &monitor);
+    RECT rect = monitor.rcMonitor;
+    SetWindowPos(hWnd, nullptr, static_cast<int>(rect.left), static_cast<int>(rect.top), static_cast<int>(rect.left + rect.right), static_cast<int>(rect.top + rect.bottom), SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+    SetWindowPos(hWnd, nullptr, static_cast<int>(rect.left), static_cast<int>(rect.top), static_cast<int>(rect.left + rect.right), static_cast<int>(rect.top + rect.bottom), 0);
 
-   // Set the window to be the size of the monitor
-   MONITORINFO monitor = {};
-   monitor.cbSize = sizeof(monitor);
-   GetMonitorInfo(MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST), &monitor);
-   RECT rect = monitor.rcMonitor;
-   SetWindowPos(hWnd, nullptr, static_cast<int>(rect.left), static_cast<int>(rect.top), static_cast<int>(rect.left + rect.right), static_cast<int>(rect.top + rect.bottom), SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
-   SetWindowPos(hWnd, nullptr, static_cast<int>(rect.left), static_cast<int>(rect.top), static_cast<int>(rect.left + rect.right), static_cast<int>(rect.top + rect.bottom), 0);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
+    return TRUE;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
+static bool isKnownAssetExtension(const std::string& ext)
+{
+    static const char* kKnown[] = {
+        // 3-D models
+        ".gltf", ".glb", ".fbx", ".obj", ".stl", ".blend",
+        // Textures
+        ".png", ".jpg", ".jpeg", ".dds", ".tga", ".bmp", ".hdr",
+        // Audio
+        ".wav", ".mp3", ".ogg",
+        // Scenes / data
+        ".json",
+    };
+    for (auto* e : kKnown) if (ext == e) return true;
+    return false;
+}
+
+static std::string handleDroppedFile(const char* srcPath)
+{
+    if (!app || !app->getAssets() || !app->getFileSystem()) return "";
+
+    namespace fs = std::filesystem;
+    ModuleFileSystem* fsys = app->getFileSystem();
+
+    std::string ext = fs::path(srcPath).extension().string();
+    for (char& c : ext) c = (char)tolower((unsigned char)c);
+
+    if (!isKnownAssetExtension(ext))
+    {
+        LOG("Drop: Unsupported file type '%s' (%s)", ext.c_str(), srcPath);
+        return "";
+    }
+
+    std::string assetsRoot = fsys->GetAssetsPath();  
+    std::string subDir;
+    if (ext == ".gltf" || ext == ".glb" || ext == ".fbx" || ext == ".obj" || ext == ".stl" || ext == ".blend")
+        subDir = "Models/";
+    else if (ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".dds" || ext == ".tga" || ext == ".bmp" || ext == ".hdr")
+        subDir = "Textures/";
+    else if (ext == ".wav" || ext == ".mp3" || ext == ".ogg")
+        subDir = "Audio/";
+
+    std::string destDir = assetsRoot + subDir;
+    fsys->CreateDir(destDir.c_str());
+
+    std::string filename = fs::path(srcPath).filename().string();
+    std::string destPath = destDir + filename;
+
+    bool alreadyInAssets = (std::string(srcPath).rfind(assetsRoot, 0) == 0);
+    if (!alreadyInAssets)
+    {
+        if (!fsys->Copy(srcPath, destPath.c_str()))
+        {
+            LOG("Drop: Failed to copy '%s' -> '%s'", srcPath, destPath.c_str());
+            return "";
+        }
+        LOG("Drop: Copied '%s' -> '%s'", srcPath, destPath.c_str());
+    }
+    else
+    {
+        destPath = srcPath;  
+    }
+
+    if (ext == ".gltf" || ext == ".glb" || ext == ".fbx" || ext == ".obj" || ext == ".stl" || ext == ".blend" ||
+        ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".dds" || ext == ".tga" || ext == ".bmp" || ext == ".hdr")
+    {
+        UID uid = app->getAssets()->importAsset(destPath.c_str());
+        if (uid != 0) {
+            LOG("Drop: Imported '%s' (uid=%llu)", destPath.c_str(), uid);
+        }
+        else {
+            LOG("Drop: Import returned 0 for '%s'", destPath.c_str());
+        }
+    }
+    else
+    {
+        app->getAssets()->refreshAssets();
+    }
+
+    return destPath;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     if (ImGui_ImplWin32_WndProcHandler(hWnd, message, wParam, lParam))
@@ -191,27 +239,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         Mouse::ProcessMessage(message, wParam, lParam);
         break;
     case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
         {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
         }
-        break;
+    }
+    break;
     case WM_PAINT:
-        {
-            app->update();
-        }
-        break;
+    {
+        app->update();
+    }
+    break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -224,13 +271,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             app->getD3D12()->resize();
         }
         break;
+
     case WM_DROPFILES:
     {
         HDROP hDrop = (HDROP)wParam;
-
         UINT fileCount = DragQueryFile(hDrop, 0xFFFFFFFF, nullptr, 0);
 
-        for (UINT i = 0; i < fileCount; i++)
+        for (UINT i = 0; i < fileCount; ++i)
         {
             WCHAR wideFilePath[MAX_PATH];
             DragQueryFileW(hDrop, i, wideFilePath, MAX_PATH);
@@ -238,55 +285,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             char narrowPath[MAX_PATH];
             WideCharToMultiByte(CP_UTF8, 0, wideFilePath, -1, narrowPath, MAX_PATH, nullptr, nullptr);
 
-            std::string filePath(narrowPath);
-            size_t dotPos = filePath.find_last_of('.');
-            if (dotPos == std::string::npos)
-                continue; 
-
-            std::string extension = filePath.substr(dotPos);
-
-            for (char& c : extension)
-                c = (char)tolower(c);
-
-            if (extension == ".gltf" || extension == ".glb" || extension == ".fbx")
-            {
-                if (app && app->getAssets())
-                {
-                    app->getAssets()->importAsset(narrowPath);
-                    LOG("? Imported model: %s", narrowPath);
-                }
-            }
-            else if (extension == ".json")
-            {
-                if (app && app->getEditor())
-                {
-                    auto* sceneManager = app->getEditor()->getSceneManager();
-                    if (sceneManager && sceneManager->getActiveScene())
-                    {
-                        if (sceneManager->loadScene(narrowPath))
-                        {
-                            LOG("? Loaded scene: %s", narrowPath);
-                        }
-                        else
-                        {
-                            LOG("? Failed to load scene: %s", narrowPath);
-                        }
-                    }
-                }
-            }
-            else if (extension == ".png" || extension == ".jpg" || extension == ".jpeg" || extension == ".dds")
-            {
-                LOG("Texture import not yet supported: %s", narrowPath);
-            }
-            else
-            {
-                LOG("Unsupported file type: %s", extension.c_str());
-            }
+            handleDroppedFile(narrowPath);
         }
 
         DragFinish(hDrop);
+
+        if (app && app->getAssets())
+            app->getAssets()->refreshAssets();
+
         return 0;
     }
+
     case WM_SYSKEYDOWN:
         if (wParam == VK_RETURN && (lParam & 0x60000000) == 0x20000000)
         {
@@ -315,7 +324,6 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     {
     case WM_INITDIALOG:
         return (INT_PTR)TRUE;
-
     case WM_COMMAND:
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
