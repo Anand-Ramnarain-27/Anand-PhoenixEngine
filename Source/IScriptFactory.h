267ac97ec7fd86d4069dd31c5b6a3b0a50dde3ca
Anand-Ramnarain-27 <142887960+Anand-Ramnarain-27@users.noreply.github.com>
2026-03-26 18:46:40 +0100

    Revert "add script interface headers"
    This reverts commit fec10900901aec4bfc0e3d1e9f0b761ca3270f90.

--- Source/IScriptFactory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IScript.h"
-
-// Every DLL must export a function with this exact signature.
-// The engine locates it via GetProcAddress.
-// Naming: Create_<ClassName>
-//
-// Example: IScript* Create_PlayerScript();
-using ScriptFactoryFn = IScript * (*)();
--- Source/ScriptExport.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef GAMESCRIPTS_EXPORTS
-#define SCRIPT_API __declspec(dllexport)
-#else
-#define SCRIPT_API __declspec(dllimport)
-#endif
